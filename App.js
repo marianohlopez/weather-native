728f577b1b679771931f3c3da535863e09c1a23d
mlopez <ml.3012@gmail.com>
2024-02-07 15:50:41 -0300

    environment-variables

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native"
+import * as Location from "expo-location"
 import Tabs from "./src/components/Tabs";
+import { TEST_KEY } from '@env'
+
 
 const App = () => {
   const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+  console.log(TEST_KEY);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== "granted") {
+        setError("permision to access location was denied")
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })
+  }, [])
+
+  if (location) {
+    console.log(location);
+  }
+
   if (loading) {
     return (
       <View style={styles.container}>
@@ -12,6 +34,7 @@ const App = () => {
       </View>
     )
   }
+
   return (
     <NavigationContainer>
       <Tabs />
